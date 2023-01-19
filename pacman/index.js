1fd69fbe90022f95b004dfefb5c78eb3ea0a7c12
Helen Lehrer <hlehrer@outlook.com>
2023-01-19 09:50:19 -0800

    Add boundary class

--- pacman/index.js
+++ pacman/index.js
@@ -1,5 +1,19 @@
 const canvas = document.querySelector('canvas');
 const c =canvas.getContext('2d');
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Boundary {
+  constructor({ position }) {
+    this.position = position;
+    this.width = 40;
+    this.height = 40;
+  }
+
+  draw() {
+    c.fillStyle = 'blue'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+//@13:42 https://www.youtube.com/watch?v=5IMXpp3rohQ
