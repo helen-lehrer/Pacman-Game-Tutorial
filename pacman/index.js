7a06e11d1f6fdf46a1c250a0352b8da5b84855c2
Helen Lehrer <hlehrer@outlook.com>
2023-01-24 21:40:45 -0800

    Add collision detection

--- pacman/index.js
+++ pacman/index.js
@@ -70,11 +70,11 @@ const keys = {
 let lastKey = '';
 
 const map = [
-  ['-', '-', '-', '-', '-', '-'],
-  ['-', ' ', ' ', ' ', ' ', '-'],
-  ['-', ' ', '-', '-', ' ', '-'],
-  ['-', ' ', ' ', ' ', ' ', '-'],
-  ['-', '-', '-', '-', '-', '-']
+  ['-', '-', '-', '-', '-', '-', '-'],
+  ['-', ' ', ' ', ' ', ' ', ' ', '-'],
+  ['-', ' ', '-', '', '-', ' ', '-'],
+  ['-', ' ', ' ', ' ', ' ', ' ', '-'],
+  ['-', '-', '-', '-', '-', '-', '-']
 ];
 
 map.forEach((row, i) => {
@@ -92,25 +92,50 @@ map.forEach((row, i) => {
   })
 })
 
+function circleCollidesWithRectangle({
+  circle,
+  rectangle
+}) {
+  return (
+    circle.position.y - circle.radius + circle.velocity.y
+      <=
+     rectangle.position.y + rectangle.height && circle.position.x + circle.radius + circle.velocity.x
+      >=rectangle.position.x && circle.position.y + circle.radius + circle.velocity.y
+      >=rectangle.position.y && circle.position.x - circle.radius + circle.velocity.x
+      <=rectangle.position.x + rectangle.width) 
+}
+
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height)
-  boundaries.forEach(boundary => {
+
+  if (keys.w.pressed && lastKey === 'w') {
+    player.velocity.y = -5
+  } else if (keys.a.pressed && lastKey === 'a') {
+    player.velocity.x = -5
+  } else if (keys.s.pressed && lastKey === 's') {
+    player.velocity.y = 5
+  } else if (keys.d.pressed && lastKey === 'd') {
+    player.velocity.x = 5
+  }
+
+  boundaries.forEach((boundary) => {
   boundary.draw()
+
+  if (
+    player.position.y - player.radius + player.velocity.y
+      <=
+      boundary.position.y + boundary.height && player.position.x + player.radius + player.velocity.x
+      >= boundary.position.x && player.position.y + player.radius + player.velocity.y
+      >= boundary.position.y && player.position.x - player.radius + player.velocity.x
+      <= boundary.position.x + boundary.width) {
+    player.velocity.x = 0;
+    player.velocity.y = 0;
+  }
 })
 player.update();
-player.velocity.y = 0
-player.velocity.x = 0
-
-if (keys.w.pressed && lastKey === 'w') {
-  player.velocity.y = -5
-} else if (keys.a.pressed && lastKey === 'a') {
-  player.velocity.x = -5
-} else if (keys.s.pressed && lastKey === 's') {
-  player.velocity.y = 5
-} else if (keys.d.pressed && lastKey === 'd') {
-  player.velocity.x = 5
-}
+// player.velocity.y = 0
+// player.velocity.x = 0
 }
 
 animate()
